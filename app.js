bd43b1f1080810dbfd51d6e501b298e9b1f8be1b
kopki684 <kopki0720@gmail.com>
2022-02-17 10:53:19 +0800

    commit

--- app.js
+++ app.js
@@ -1,235 +1,166 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let canvas = document.getElementById('canvas')
-    let ctx = canvas.getContext('2d');
-    
-    let x = 10 // x起始
-    let y = 40 // y起始
-    let snake = [
-        {
-            x: 10,
-            y: 40
-        }
-    ]
-
-    let appleDirection = {
-        x: 70,
+let snake = [
+    {
+        x: 10,
         y: 40
-    } 
+    }
+];
 
-    let timeSpace = 150
-    let direction = null;
-    let timer;
+let x = snake[0].x
+let y = snake[0].y
 
-    if (canvas.getContext) {
-        for (let i = 0; i < snake.length; i++) {
-            createSnake(ctx, snake[i].x,  snake[i].y)
-        }
+let applePosition = {
+    x: 70,
+    y: 40
+};
+
+let direction = null;
+let timer;
+
+document.addEventListener('DOMContentLoaded', () => {
+    let canvas = document.querySelector('canvas');
+    let ctx = canvas.getContext('2d');
+
+    if(canvas.getContext) {
+        drawRect(ctx, snake[0].x,  snake[0].y, 'black');
+        drawRect(ctx, applePosition.x,  applePosition.y, 'red');
     }
 
-    createApple(ctx, appleDirection.x, appleDirection.y)
-
-    document.addEventListener('keydown', decideDirection)
+    document.addEventListener('keydown', decideDirection);
 
     function decideDirection(e) {
-        const code = e.code
-        clearInterval(timer)
-
-        switch (code) {
+        clearInterval(timer);
+    
+        switch (e.code) {
             case "ArrowUp":
                 if(direction === "ArrowDown") break;
-                direction = "ArrowUp"
+                direction = "ArrowUp";
                 break;
             case "ArrowDown":
                 if(direction === "ArrowUp") break;
-                direction = "ArrowDown"
+                direction = "ArrowDown";
                 break;
             case "ArrowLeft":
                 if(direction === "ArrowRight") break;
-                direction = "ArrowLeft"
+                direction = "ArrowLeft";
                 break;
             case "ArrowRight":
                 if(direction === "ArrowLeft") break;
-                direction = "ArrowRight"
+                direction = "ArrowRight";
                 break;
         }
-
+    
         timer = setInterval(()=> {
-            move(direction)
-            createApple(ctx, appleDirection.x, appleDirection.y)
+            move()
+            drawRect(ctx, applePosition.x, applePosition.y, 'red')
             checkGameOver()
-            
-            eatApple(direction)
-            
-        }, timeSpace)
+            eatApple()
+        }, 150)   
     }
-
-    function move(direction) {
-        
-
+    
+    function move() {
         switch (direction) {
             case "ArrowUp":
-                
-                y -= 10
-                snake.unshift({x: x, y: y})
-                snake.pop()
-
-                ctx.clearRect(0, 0, 100, 100)
-                for (let i = 0; i < snake.length; i++) {
-                    createSnake(ctx, snake[i].x,  snake[i].y)
-                }
-                
+                y -= 10;
+                createSnake()
                 break;
             case "ArrowDown":
-                
-
                 y += 10
-
-                snake.unshift({x: x, y: y})
-                snake.pop()
-
-                ctx.clearRect(0, 0, 100, 100)
-                for (let i = 0; i < snake.length; i++) {
-                    createSnake(ctx, snake[i].x,  snake[i].y)
-                }
-                
+                createSnake()
                 break;
             case "ArrowLeft":
                 x -= 10
-
-                snake.unshift({x: x, y: y})
-                snake.pop()
-
-                ctx.clearRect(0, 0, 100, 100)
-                for (let i = 0; i < snake.length; i++) {
-                    createSnake(ctx, snake[i].x,  snake[i].y)
-                }
-                
+                createSnake()
                 break;
             case "ArrowRight":
-               
-
                 x += 10
-
-                snake.unshift({x: x, y: y})
-                snake.pop()
-
-                ctx.clearRect(0, 0, 100, 100)
-                for (let i = 0; i < snake.length; i++) {
-                    createSnake(ctx, snake[i].x,  snake[i].y)
-                }
-                
-                
+                createSnake()
                 break;
         }
     }
-
-    function eatApple(direction) {
-        if(x !== appleDirection.x || y !== appleDirection.y) return;
     
-        // switch (direction) {
-        //     case "ArrowUp":
-        //         y -= 10
-        //         snake.unshift({x: x, y: y})
-        //         break;
-        //     case "ArrowDown":
-        //         y += 10
-        //         snake.unshift({x: x, y: y})
-        //         break;
-        //     case "ArrowLeft":
-        //         x -= 10
-        //         snake.unshift({x: x, y: y})
-        //         break;
-        //     case "ArrowRight":
-        //         x += 10
-        //         snake.unshift({x: x, y: y})
-        //         break;
-        // }
-
+    function createSnake() {
+        snake.unshift({
+            x,
+            y
+        });
+        snake.pop();
+    
+        ctx.clearRect(0, 0, 100, 100);
+        for (let i = 0; i < snake.length; i++) {
+            drawRect(ctx, snake[i].x,  snake[i].y, 'black')
+        }       
+    }
+    
+    function eatApple() {
+        if(x !== applePosition.x || y !== applePosition.y) return;
+    
         snake.push({x: snake[snake.length - 1].x - 10, y: snake[snake.length - 1].y})
-        let newAppleX = getRandomNumber(10) * 10
-        let newAppleY = getRandomNumber(10) * 10
-
-        let repeatLocation = snake.some(tile => {
-            return tile.x === newAppleX && tile.y === newAppleY
-        })
-
-        while(repeatLocation) {
-            newAppleX = getRandomNumber(10) * 10
-            newAppleY = getRandomNumber(10) * 10
-
-            repeatLocation = snake.some(tile => {
-                return tile.x === newAppleX && tile.y === newAppleY
-            })
+    
+        let newAppleX = getRandomNumber(10) * 10;
+        let newAppleY = getRandomNumber(10) * 10;
+    
+        while(checkRepeatLocation()) {
+            newAppleX = getRandomNumber(10) * 10;
+            newAppleY = getRandomNumber(10) * 10;
         }
-
-        appleDirection = {
+    
+        applePosition = {
             x: newAppleX,
             y: newAppleY
         }
         
+        function checkRepeatLocation() {
+            return snake.some(tile => {
+                return tile.x === newAppleX && tile.y === newAppleY;
+            })
+        }
     }
     
-
     function checkGameOver() {
         let repeatLocation = snake.some((tile, index) => {
-            if(index === 0) return false
-            return tile.x === x && tile.y === y
+            if(index === 0) return false;
+            return tile.x === x && tile.y === y;
         })
-
-
+    
         if(x < 0 || x >= 100 || y < 0 || y >= 100 || repeatLocation ) {
-            alert('Game Over 🐍')
-            clearInterval(timer)
-            direction = null
-            x = 10;
-            y = 40;
+            alert('Game Over 🐍');
+            clearInterval(timer);
+            
             snake = [
                 {
                     x: 10,
                     y: 40
                 }
-            ]
-
-            appleDirection = {
+            ];
+    
+            x = snake[0].x;
+            y = snake[0].y;
+    
+            applePosition = {
                 x: 70,
                 y: 40
+            };
+            direction = null;
+            ctx.clearRect(0, 0, 100, 100);
+    
+            if(canvas.getContext) {
+                drawRect(ctx, snake[0].x,  snake[0].y, 'black');
+                drawRect(ctx, applePosition.x,  applePosition.y, 'red');
             }
-            ctx.clearRect(0, 0, 100, 100)
-
-            if (canvas.getContext) {
-                for (let i = 0; i < snake.length; i++) {
-                    createSnake(ctx, snake[i].x,  snake[i].y)
-                }
-            }
-        
-            createApple(ctx, appleDirection.x, appleDirection.y)
         }
     }
 })
 
-function createSnake(ctx, x, y) {
+function drawRect(ctx, x, y, color) {
     ctx.beginPath();
     ctx.moveTo(x, y);
     ctx.lineTo(x + 10, y);
     ctx.lineTo(x + 10, y + 10);
     ctx.lineTo(x, y + 10);
-    ctx.fillStyle = 'black'
+    ctx.fillStyle = color;
     ctx.fill();
 }
-
-function createApple(ctx, x, y) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + 10, y);
-    ctx.lineTo(x + 10, y + 10);
-    ctx.lineTo(x, y + 10);
-    ctx.fillStyle = 'red'
-    ctx.fill();
-}
-
 
 function getRandomNumber(max) {
     return Math.floor(Math.random() * max);
 }
-
-
